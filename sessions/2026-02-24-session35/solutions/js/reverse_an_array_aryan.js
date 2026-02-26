a8f070fe4d2f86e6964b8355d531f5361b593a03
Sharmaryan <panditaryan.as@gmail.com>
2026-02-26 11:50:37 +0530

    reverse an array

---
+++ sessions/2026-02-24-session35/solutions/js/reverse_an_array_aryan.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @returns {void}
+ */
+
+class Solution {
+    rev(arr, i, j) {
+        if (i < j) {
+            [arr[j], arr[i]] = [arr[i], arr[j]]
+            return this.rev(arr, i + 1, j - 1)
+        }
+    }
+
+    reverseArray(arr) {
+        this.rev(arr, 0, arr.length - 1)
+        return arr
+    }
+}
